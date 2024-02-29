1635e3ab6b6dcfb0f11927d659a93ef832d04fe5
gonzalez-lourdnathaniel <gonzalez.lourdnathaniel@gmail.com>
2024-02-29 10:27:55 +0800

    Created route template

---
+++ routes/quizRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const quizController = require('../controllers/quizController');
+
+
+
+module.exports = router;
---
+++ routes/quizSetRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const quizSetController = require('../controllers/quizSetController');
+
+
+
+module.exports = router;
