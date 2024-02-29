c582cd2f5af4ac3e514fa44c218853bcdcd4f747
gonzalez-lourdnathaniel <gonzalez.lourdnathaniel@gmail.com>
2024-02-29 10:28:37 +0800

    Created CRUD template

---
+++ controllers/questionController.js
@@ -0,0 +1,13 @@
+/**
+ * TODO: Write up the necessary functions under the questions controller.
+ * TODO: Write the CRUD functions necessary and connect to SQL RDBMs.
+ * TODO: Provide the documentation for each functions.
+ */
+
+module.exports.createQuestion = async () => {};
+
+module.exports.viewQuestion = async () => {};
+
+module.exports.editQuestion = async () => {};
+
+module.exports.deleteQuestion = async () => {};
---
+++ controllers/quizSetController.js
@@ -0,0 +1,13 @@
+/**
+ * TODO: Write up the necessary functions under the quiz set controller.
+ * TODO: Write the CRUD functions necessary and connect to SQL RDBMs.
+ * TODO: Provide the documentation for each functions.
+ */
+
+module.exports.createQuizSet = async () => {};
+
+module.exports.viewQuizSet = async () => {};
+
+module.exports.editQuizSet = async () => {};
+
+module.exports.deleteQuizSet = async () => {};
