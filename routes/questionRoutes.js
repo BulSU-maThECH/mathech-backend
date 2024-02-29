f05cee955978e71725a931cfa64f5cf42557a680
gonzalez-lourdnathaniel <gonzalez.lourdnathaniel@gmail.com>
2024-02-29 10:35:12 +0800

    Finished setup of routes

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const questionController = require('../controllers/questionController');
+const auth = require('../middleware/Authentication');
 
+router.post('/create', (req, res) => {
+    questionController.createQuestion(req.body).then(resultFromController => res.send(resultFromController));
+});
 
+router.get('/view/:id', (req, res) => {
+    questionController.viewQuestion(req.params).then(resultFromController => res.send(resultFromController));
+});
+
+router.patch('/edit/:id', auth.verifyUser, (req, res) => {
+    questionController.editQuestion(req.params, req.body).then(resultFromController => res.send(resultFromController));
+});
+
+router.delete('/delete/:id', auth.verifyUser, (req, res) => {
+    questionController.deleteQuestion(req.params).then(resultFromController => res.send(resultFromController));
+});
 
 module.exports = router;
--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const quizController = require('../controllers/quizController');
+const auth = require('../middleware/Authentication');
 
+router.post('/create', (req, res) => {
+    quizController.createQuiz(req.body).then(resultFromController => res.send(resultFromController));
+});
 
+router.get('/view/:id', (req, res) => {
+    quizController.viewQuiz(req.params).then(resultFromController => res.send(resultFromController));
+});
+
+router.patch('/edit/:id', auth.verifyUser, (req, res) => {
+    quizController.editQuiz(req.params, req.body).then(resultFromController => res.send(resultFromController));
+});
+
+router.delete('/delete/:id', auth.verifyUser, (req, res) => {
+    quizController.deleteQuiz(req.params).then(resultFromController => res.send(resultFromController));
+});
 
 module.exports = router;
